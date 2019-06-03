e24e4627c876a870e4070bf7862e706ed1821d94
javier Martinez <00034315@uca.edu.sv>
2019-06-03 03:36:08 -0600

    cambios finales, crea el partido pero no deja cambiar puntaje

--- app/src/main/java/com/example/parcial1/adapters/MatchAdapter.kt
+++ app/src/main/java/com/example/parcial1/adapters/MatchAdapter.kt
@@ -37,17 +37,16 @@ class MatchAdapter(var items: List<Match>, var listener: View.OnClickListener): 
         fun bind(item: Match) = with(itemView){
             tv_teamA.text = item.TeamA
             tv_teamB.text = item.TeamB
-            tv_scoreA.text = item.ScoreA.toString()
-            tv_scoreB.text = item.ScoreB.toString()
+
             if(item.ScoreB > item.ScoreA){
-                tv_scoreB.setTextColor(Color.parseColor("#229922"))
-                tv_scoreA.setTextColor(Color.parseColor("#CC2222"))
+                tv_scoreB.setTextColor(Color.parseColor("#000000"))
+                tv_scoreA.setTextColor(Color.parseColor("#000000"))
                 if(item.MatchEnd) {
                     tv_result.text = "Ganador: " + item.TeamB
                 }
             }else if(item.ScoreB< item.ScoreA){
-                tv_scoreA.setTextColor(Color.parseColor("#229922"))
-                tv_scoreB.setTextColor(Color.parseColor("#CC2222"))
+                tv_scoreA.setTextColor(Color.parseColor("#000000"))
+                tv_scoreB.setTextColor(Color.parseColor("#000000"))
                 if(item.MatchEnd) {
                     tv_result.text = "Ganador: " + item.TeamA
                 }
@@ -58,15 +57,15 @@ class MatchAdapter(var items: List<Match>, var listener: View.OnClickListener): 
 
             val calendar = Calendar.getInstance()
             calendar.time = item.date
-            tv_date.text = "Fecha: "+calendar.get(Calendar.DAY_OF_MONTH).toString()+"/"+(calendar.get(Calendar.MONTH)+1).toString()+"/"+calendar.get(Calendar.YEAR).toString()
+            tv_date.text = "DATE: "+calendar.get(Calendar.DAY_OF_MONTH).toString()+"/"+(calendar.get(Calendar.MONTH)+1).toString()
 
-            var minutos = ""
+            var minutes = ""
             if(calendar.get(Calendar.MINUTE).toString().length == 1){
-                minutos = "0"+calendar.get(Calendar.MINUTE).toString()
+                minutes = "0"+calendar.get(Calendar.MINUTE).toString()
             }else{
-                minutos = calendar.get(Calendar.MINUTE).toString()
+                minutes = calendar.get(Calendar.MINUTE).toString()
             }
-            tv_time.text = "Hora: "+calendar.get(Calendar.HOUR_OF_DAY).toString()+":"+minutos
+            tv_time.text = "TIME: "+calendar.get(Calendar.HOUR_OF_DAY).toString()+":"+minutes
         }
 
     }
